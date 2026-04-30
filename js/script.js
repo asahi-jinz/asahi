54002a9d1872d45d341d0a4670abe62d4a033b19
asahi-jinz <asahi.jinz.office@gmail.com>
2026-04-30 16:35:23 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-
+$(document).on('ready', function() {
+  $(".full").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    speed: 1500,
+    fade: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+  });
+});
