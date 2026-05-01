4f83fe78d0d076cdc7e8cbca0089b9cb55bc027f
asahi-jinz <asahi.jinz.office@gmail.com>
2026-05-01 09:00:02 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-$(document).on('ready', function() {
-  $(".full").slick({
-    arrows: false,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    speed: 1500,
-    fade: true,
-    pauseOnFocus: false,
-    pauseOnHover: false,
-    pauseOnDotsHover: false,
-  });
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  effect: "fade", // フェード切り替え
+  // 自動再生
+  autoplay: {
+    delay: 4000, // 4秒後に次のスライドへ
+    disableOnInteraction: false, // ユーザーが操作しても自動再生を継続
+  },
+  speed: 2000, // 2秒かけてフェード
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
